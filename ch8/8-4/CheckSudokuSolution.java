5526ad0c5bf4143e1bc8a51bebe9ed20273519c7
ZhengkaiXu <695378091@qq.com>
2024-10-06 21:20:12 +0800

    8-4

---
+++ ch8/8-4/CheckSudokuSolution.java
@@ -0,0 +1,11 @@
+/**
+ * @Author: xzk@arcvideo.com
+ * @Date: 2024/10/6 21:18
+ * @Description:
+ */
+import java.util.Scanner;
+public class CheckSudokuSolution {
+    public static void main(String[] args) {
+        System.out.print("hello!");
+    }
+}
